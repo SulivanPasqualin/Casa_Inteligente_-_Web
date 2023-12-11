1afcd75c313b22fb00d18b22b6f08397adee299a
Sulivan <sulivanpasqualin@live.com>
2023-12-11 19:13:09 -0300

    Views e Routes

--- server.js
+++ server.js
@@ -1,30 +1,70 @@
-const express = require("express")
-const servidor = express()
+// const express = require("express")
+// const servidor = express()
+// const path = require('path');
+
+// servidor.use(express.static(path.join(__dirname, 'public')));
+
+// // Rota Login
+// servidor.get("/login",function(req,res){
+//     res.sendFile(__dirname + "/views/pages/login.html")
+// })
+
+// // Rota Home
+// servidor.get("/home",function(req,res){
+//     res.sendFile(__dirname + "/views/pages/home.html")
+// })
+
+// // Rota Locais
+// servidor.get("/locais",function(req,res){
+//     res.sendFile(__dirname + "/views/pages/locais.html")
+// })
+
+// // Rota Dispositivos
+// servidor.get("/dispositivos",function(req,res){
+//     res.sendFile(__dirname + "/views/pages/dispositivos.html")
+// })
+
+// // Servidor escutando na porta 3000
+// servidor.listen(3000,function(){
+//     console.log(`Servidor executando...`)
+// })
+
+const express = require("express");
+const servidor = express();
 const path = require('path');
 
+servidor.set('view engine', 'ejs'); // Definir EJS como o mecanismo de visualização
+servidor.set('views', path.join(__dirname, 'views')); // Define o diretório onde os arquivos de visualização serão armazenados
+
+// Servir arquivos estáticos
 servidor.use(express.static(path.join(__dirname, 'public')));
 
 // Rota Login
-servidor.get("/login",function(req,res){
-    res.sendFile(__dirname + "/views/pages/login.html")
-})
+servidor.get("/login", function(req, res) {
+    res.render('pages/login'); // Renderizar o arquivo EJS em vez de enviar o HTML diretamente
+});
 
 // Rota Home
-servidor.get("/home",function(req,res){
-    res.sendFile(__dirname + "/views/pages/home.html")
-})
+servidor.get("/home", function(req, res) {
+    res.render('pages/home');
+});
 
-// Rota Locais
-servidor.get("/locais",function(req,res){
-    res.sendFile(__dirname + "/views/pages/locais.html")
-})
+// Rota comodos
+servidor.get("/comodos", function(req, res) {
+    res.render('pages/comodos');
+});
+
+// Rota locais
+servidor.get("/locais", function(req, res) {
+    res.render('pages/locais');
+});
 
 // Rota Dispositivos
-servidor.get("/dispositivos",function(req,res){
-    res.sendFile(__dirname + "/views/pages/dispositivos.html")
-})
+servidor.get("/dispositivos", function(req, res) {
+    res.render('pages/dispositivos');
+});
 
 // Servidor escutando na porta 3000
-servidor.listen(3000,function(){
-    console.log(`Servidor executando...`)
-})
+servidor.listen(3000, function() {
+    console.log(`Servidor executando...`);
+});
